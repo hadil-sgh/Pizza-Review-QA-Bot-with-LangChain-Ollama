e2f2dd366974569ccca655b5ed3ada70d4282760
Hadil Sghair <hadil03sghair@gmail.com>
2025-05-04 18:21:30 +0100

    Feat: Impemented a Llama model to answer questions from terminal + vectorized and embedded a csv file to the model for better results

--- vector.py
+++ vector.py
@@ -7,7 +7,7 @@ import pandas as pd
 df = pd.read_csv("realistic_restaurant_reviews.csv")
 embeddings = OllamaEmbeddings(model="mxbai-embed-large")
 
-# A place to store the vector database
+# ? A place to store the vector database
 db_location = "chroma_langchain_db"
 add_documents =  not os.path.exists(db_location) # to granite that we don't overwrite the database if it already exists
 
@@ -15,7 +15,7 @@ if add_documents:
     Documents = []
     ids = []
     for i, row in df.iterrows():
-        # Create a Document object for each review
+        # ?Create a Document object for each review
         doc = Document(
             page_content=row["Title"] +"²" + row["Review"],
             metadata={"rating": row["Rating"], "date": row["Date"]},
@@ -33,13 +33,13 @@ vector_store = Chroma(
         
         
 if add_documents:
-    # Add the documents to the vector store
+    #? Add the documents to the vector store
     vector_store.add_documents(documents= Documents, ids=ids)
     # Persist the database to disk
   
         
   
-# Load the vector store from disk      
+# ? Load the vector store from disk      
 retriever = vector_store.as_retriever(
     search_kwargs={"k": 10} # Number of documents / reviews to retrieve 
     )        
